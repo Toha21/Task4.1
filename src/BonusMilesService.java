8c2050a10acae7f1a9528768df2c1dafdbe5ebe9
Anton Gavryukhin <ah21@mail.ru>
2022-08-05 20:42:36 +0300

    Revisión commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int cost) {
-        int calculate = cost / 20;
-        return calculate;
-
-
+        int miles = cost / 20;
+        return miles;
     }
-
-
-
-    }
+}
